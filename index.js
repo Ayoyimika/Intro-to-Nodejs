1068efc8b0c824db49bfa51809bc9d788173720d
unknown <vicaava@gmail.com>
2023-03-29 12:04:32 +0100

    Commit js file

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log("My name is Victoria Ayodeji.");
+console.log("My preferred programming language is JavaScript.");
+console.log("In the next year, I hope to continue improving my skills in Js and finally bloom in Test Automation")
